f49ee335094bf25186c570fd5625df51bc5668e6
Ansh Gupta <ng607301@gmail.com>
2026-03-02 18:14:55 +0530

    create food & storage service

--- server/controllers/food-controller.js
+++ server/controllers/food-controller.js
@@ -1,10 +1,28 @@
-const foodModel = require("../models/food-model")
-const createFood = (req,res)=>{
-    try {
-        console.log(req.foodPartner)
-    } catch (error) {
-        
-    }
-}
+const foodModel = require("../models/food-model");
+const { v4: uuid } = require("uuid");
+const StatusCode = require("../utils/status-code");
+const storageService = require("../services/storage-service");
+// create a food with video
+const createFood = async (req, res) => {
+  try {
+    const fileUploadResult = await storageService.uploadFile(
+      req.file.buffer,
+      uuid(),
+    );
+    const foodItem = await foodModel.create({
+      name: req.body.name,
+      video: fileUploadResult.url,
+      description: req.body.description,
+      foodPartner: req.foodPartner._id,
+    });
+    return res
+      .status(StatusCode.CREATED)
+      .json({ message: "Food item is created", data: foodItem });
+  } catch (error) {
+    return res
+      .status(StatusCode.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
+  }
+};
 
-module.exports = {createFood}
+module.exports = { createFood };
--- server/routes/food-routes.js
+++ server/routes/food-routes.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 const {createFood} = require("../controllers/food-controller")
 const {authFoodPartnerMiddleware} = require("../middleware/auth-middleware")
+const multer = require('multer')
 
-router.post('/',authFoodPartnerMiddleware,createFood)
+// multer logic
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+router.post('/',authFoodPartnerMiddleware,upload.single("video"),createFood)
 
 module.exports = router
